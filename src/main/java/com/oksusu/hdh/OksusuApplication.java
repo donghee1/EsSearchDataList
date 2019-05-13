081210599a0aa22079a6851368880c02448d7a24
[지나웍스] 한동희 <sms445566@zinnaworks.com>
2019-05-13 11:24:04 +0900

    어플리케이션 페이지 스프링부트서블릿인티저라이저 상속 추가, 빌드그레이들 이클립스 주석 해제

--- src/main/java/com/oksusu/hdh/OksusuApplication.java
+++ src/main/java/com/oksusu/hdh/OksusuApplication.java
@@ -2,13 +2,14 @@ package com.oksusu.hdh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 //@MapperScan({"com.oksusu.hdh.mapper"})
 @EnableScheduling
-public class OksusuApplication  {
+public class OksusuApplication extends SpringBootServletInitializer  {
 	
 
 	public static void main(String[] args) {
